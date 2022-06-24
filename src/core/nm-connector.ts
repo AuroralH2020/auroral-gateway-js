a71f27b6fac475f2a45091304ba233371c97f860
Peter Drahovsky <peter.drahovsky@bavenir.eu>
2022-06-24 11:48:28 +0200

    Update: - nm-connector calls updated

--- src/core/nm-connector.ts
+++ src/core/nm-connector.ts
@@ -30,6 +30,7 @@ const ApiHeader = {
 }
 
 // INTERFACE
+type testType = string
 
 export const nm = {
 
@@ -49,6 +50,175 @@ export const nm = {
             logger.error('Initial handshake failed...')
             throw new Error(error.message)
         }
+    },
+    /**
+     *  Retrieve counters from NM
+     * @returns {error?: string, message: string}
+     */
+    getCounters: async function(): Promise<GenericResponse<string>> {
+        const msg = await request('counters', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     *  Post counters to NM
+     * @param records 
+     * @returns jsonType
+     */
+    postCounters: async function(records: JsonType): Promise<GenericResponse<void>> {
+        const msg = await request('counters', 'POST', records, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get user roster from NM
+     * @param oid 
+     * @returns jsonType
+     */
+    getItemRoster: async function(oid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request('items/' + oid, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     *  Register item in NM
+     * @param agid 
+     * @param items 
+     * @returns jsonType
+     */
+    registerItems: async function(agid: string, items: JsonType[]): Promise<GenericResponse<JsonType>> {
+        const msg = await request('items/register', 'POST', { agid, items }, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     *  Remove item from NM
+     * @param agid 
+     * @param oids 
+     * @returns jsonType
+     */
+    removeItems: async function(agid: string, oids: string[]): Promise<GenericResponse<JsonType>> {
+        const msg = await request('items/remove', 'POST', { agid, oids }, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Update item in NM
+     * @param agid 
+     * @param oids 
+     * @returns jsonType
+     */
+    modifyItems: async function(agid: string, oids: JsonType[]): Promise<GenericResponse<JsonType>> {
+        const msg = await request('items/modify', 'PUT', { agid, oids }, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Delete agent from NM
+     * @param agid 
+     * @returns jsonType
+     */
+    deleteAgent: async function(agid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request('agent/' + agid, 'DELETE', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get agent items from NM
+     * @param agid 
+     * @returns jsonType
+     */
+    getAgentItems: async function(agid: string): Promise<GenericResponse<JsonType>> {
+            const msg = await request(`agent/${agid}/items`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+            return msg 
+    },
+    /**
+     * Get agent privacy from NM
+     * @returns jsonType
+     */
+    getAgentPrivacy: async function(): Promise<GenericResponse<JsonType>> {
+        const msg = await request('agent/privacy', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     *  Returns cid of from oid or agid
+     * @param reqid 
+     * @returns jsonType
+     */
+    getCidFromReqid: async function(reqid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`agent/cid/${reqid}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Returns pertners
+     * @returns jsonType
+     */
+    getPartners: async function(): Promise<GenericResponse<JsonType>> {
+        const msg = await request('agent/partners', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get partner info from NM
+     * @param cid 
+     * @returns jsonType
+     */
+    getPartner: async function(cid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`agent/partner/${cid}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get contracted items by cid of remote org
+     * @param cid 
+     * @returns jsonType
+     */
+    getContractedItemsByCid: async function(cid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`agent/contract/items/${cid}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    // DISCOVERY
+    /**
+     *  Get my communities
+     * @returns jsonType
+     */
+    getCommunities: async function(): Promise<GenericResponse<JsonType>> {
+        const msg = await request('agent/communities', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get nodes in my organisation
+     * @returns jsonType
+     */
+    getNodesInMyOrganisation: async function(): Promise<GenericResponse<JsonType>> {
+        const msg = await request('discovery/nodes/organisation', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get nodes at given organisation
+     * @param cid 
+     * @returns jsonType
+     */
+    getNodesInOrganisation: async function(cid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`discovery/nodes/organisation/${cid}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get nodes in given community
+     * @param commId 
+     * @returns jsonType
+     */
+    getNodesInCommunity: async function(commId: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`discovery/nodes/comunity/${commId}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get items in my organisation
+     * @returns jsonType
+     */
+    getItemsInMyOrganisation: async function(): Promise<GenericResponse<JsonType>> {
+        const msg = await request('discovery/items/organisation', 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
+    },
+    /**
+     * Get items in given contract
+     * @param ctid 
+     * @returns jsonType
+     */
+    getItemsInContract: async function(ctid: string): Promise<GenericResponse<JsonType>> {
+        const msg = await request(`discovery/items/contract/${ctid}`, 'GET', undefined, { ...ApiHeader, 'X-ACCESS-TOKEN': 'Bearer ' + Token.token })
+        return msg
     }
 
 }
